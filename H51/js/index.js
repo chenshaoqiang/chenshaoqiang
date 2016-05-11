19fb7b5b90c68becbca910aa28ddce5c1f0d64c5
chenshaoqiang <877251132@qq.com>
2016-05-11 12:26:36 +0800

    H5 update

--- H51/js/index.js
+++ H51/js/index.js
@@ -2,8 +2,8 @@
 var desW=320;//设计稿的宽度
 var winW=document.documentElement.clientWidth;//设备的实际宽度
 var winH=document.documentElement.clientHeight;//设备的实际高度
-var main=document.getElementById("main");
+//var main=document.getElementById("main");
 var scale=320/100;
 var allFontSize=document.documentElement.style.fontSize=winW/scale+"px";;
-main.style.height=winH/(winW/scale)+"rem";
-console.log(allFontSize,main.style.height);
+//main.style.height=winH/(winW/scale)+"rem";
+//console.log(allFontSize,main.style.height);
