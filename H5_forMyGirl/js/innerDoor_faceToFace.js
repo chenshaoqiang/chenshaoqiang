2f40605e25d180a8b9b1ff153a2ac119f841cdaf
chenshaoqiang <877251132@qq.com>
2016-05-13 21:58:46 +0800

    H5

---
+++ H5_forMyGirl/js/innerDoor_faceToFace.js
@@ -0,0 +1,76 @@
+
+var sourceW = 320;
+var sourceH = 568;
+var windowW = document.documentElement.clientWidth;//浏览器宽
+var windowH = document.documentElement.clientHeight;//浏览器高
+var background=document.getElementById("background");
+var oLis = document.querySelectorAll("#list>li");
+
+var winW=getComputedStyle(background,null).width;//图片框宽
+var winH=getComputedStyle(background,null).height;//图片框高
+var fontScale=320/100;
+var fontSize=windowW/fontScale;
+console.log(fontSize);
+[].forEach.call(oLis, function () {
+    var oLi = arguments[0];
+    oLi.index = arguments[1];
+    oLi.addEventListener("touchstart", start, false);
+    oLi.addEventListener("touchmove", move, false);
+    oLi.addEventListener("touchend", end, false);
+});
+function start(ev) {
+    this.startX = ev.changedTouches[0].pageX;
+    this.startY = ev.changedTouches[0].pageY;
+    console.log("点到了", "X:" + this.startX, "Y:" + this.startY);
+}
+
+function move(ev) {
+
+    ev.preventDefault();
+    var nowIndex = this.index;
+    var step = 1 / 4;
+    var touchX = ev.changedTouches[0].pageX;
+    var touchY = ev.changedTouches[0].pageY;
+    var changeX = touchX - this.startX;
+    var changeY = touchY - this.startY;
+    this.flag = true;
+    //横向移动则不变
+    if (Math.abs(changeX) > Math.abs(changeY)) {
+        console.log("横向滑动，不改变");
+        this.flag = false;
+        return;
+    }
+    [].forEach.call(oLis, function () {
+        if (nowIndex != arguments[1]) {
+            arguments[0].style.display = "none";
+        }
+        arguments[0].className = "";
+    });
+    if (changeY < 0) {//向上
+        this.nextIndex = nowIndex == oLis.length - 1 ? 0 : nowIndex + 1;
+        oLis[this.nextIndex].style.webkitTransform =
+            "translate(0," + ((winH + changeY)/fontSize) + "rem)";
+        console.log("在向上移动", "Y改变：" + changeY, "下一张：" + this.nextIndex);
+    } else if ((changeY > 0)) {//向下
+        this.nextIndex = nowIndex == 0 ? oLis.length - 1 : nowIndex - 1;
+        oLis[this.nextIndex].style.webkitTransform =
+            "translate(0," + ((-winH + changeY)/fontSize) + "rem)";
+        console.log("在向下移动", "Y改变：" + changeY, "下一张：" + this.nextIndex);
+    }
+    oLis[this.nextIndex].className = "zIndex";
+    oLis[this.nextIndex].style.display = "block";
+    this.style.webkitTransform = "scale(" + (1 - Math.abs(changeY / winH) * step ) + ") translate(0,0)";
+
+}
+
+function end(ev) {
+    console.log("离开屏幕啦");
+    if (this.flag) {
+        oLis[this.nextIndex].style.webkitTransform = "translate(0,0)";
+        oLis[this.nextIndex].style.webkitTransition = "0.5s";
+        oLis[this.nextIndex].addEventListener("webkitTransitionEnd", function () {
+            this.style.webkitTransition = "";
+        }, false);
+        this.flag = false;
+    }
+}
