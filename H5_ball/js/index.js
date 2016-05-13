486a27e5cc14280f7adf8d8f473a93d10ce53c72
chenshaoqiang <877251132@qq.com>
2016-05-13 15:52:54 +0800

    H5

--- H5_ball/js/index.js
+++ H5_ball/js/index.js
@@ -1,6 +1,8 @@
-/*自适应设备调整根字体大小*/
-var desW=640;//设计稿的宽度
-var winW=document.documentElement.clientWidth;//设备的实际宽度
-var scale=640/100;
-document.documentElement.style.fontSize=winW/scale+"px";
-
+var main = document.getElementById("main");
+var desW = 640;//设计稿的宽度
+var winW = document.documentElement.clientWidth;//设备的实际宽度
+var winH = document.documentElement.clientHeight;//设备的实际宽度
+var scale = 640 / 100;
+var fontsize = document.documentElement.style.fontSize = winW / scale + "px";
+main.style.height = winH/(winW / scale)+"rem";
+console.log(main.style.height,fontsize);
