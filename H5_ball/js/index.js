08f4c55147226bb5638fddc896f855cfee8e42d9
chenshaoqiang <877251132@qq.com>
2016-05-13 16:09:28 +0800

    H5

--- H5_ball/js/index.js
+++ H5_ball/js/index.js
@@ -1,8 +1,8 @@
 var main = document.getElementById("main");
-var desW = 640;//设计稿的宽度
+var desW = 320;//设计稿的宽度
 var winW = document.documentElement.clientWidth;//设备的实际宽度
 var winH = document.documentElement.clientHeight;//设备的实际宽度
-var scale = 640 / 100;
+var scale = 320 / 100;
 var fontsize = document.documentElement.style.fontSize = winW / scale + "px";
 main.style.height = winH/(winW / scale)+"rem";
 console.log(main.style.height,fontsize);
