29730037175ac58b88743bef88b0e3532b595166
chenshaoqiang <877251132@qq.com>
2017-05-30 00:35:36 +0800

    5.29

--- zipaigan/media/js/common_directive.js
+++ zipaigan/media/js/common_directive.js
@@ -3,34 +3,40 @@
  */
 
 mainModule.directive('imgLazyLoad', function() {
+
+    var maxWidth=$("#imgBox").width()-100;
+
     //图片延迟加载
     return {
 
         restrict: 'AE',
 
         link: function(scope, element, attr) {
+
+            $(element[0]).parents(".workList");
+
             var realSrc = scope.work.worksUrl;
             var tempImg=new Image;
             tempImg.src=realSrc;
             tempImg.onload=function(){
+
+                if(parseInt(this.width)<parseInt(maxWidth)){
+                    //处理小图，比例大于70%则设100%；比例小于70%则设置50%
+                    var prop=parseInt(parseInt(this.width)/parseInt(maxWidth)*100);//百分比
+
+                    if(prop>=70){
+
+                        $(element[0]).parents(".workList").css("width",'100%');
+
+                    }else{
+
+                        $(element[0]).parents(".workList").css("width",'50%')
+
+                    }
+
+                }
                 $(element[0]).attr("src",realSrc);
             };
-            function offset(ele){
-                var t=ele.offsetTop;
-                var l=ele.offsetLeft;
-                var p=ele.offsetParent;
-                while(p){
-                    if(window.navigator.userAgent.indexOf("MSIE 8")==-1){
-                        t+=p.offsetTop+p.clientTop;
-                        l+=p.offsetLeft+p.clientLeft;
-                    }else{
-                        t+=p.offsetTop;
-                        l+=p.offsetLeft;
-                    }
-                    p=p.offsetParent;
-                }
-                return {left:l,top:t};
-            }
         }
     };
 });
